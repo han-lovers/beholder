373c9b84b9ba102d11f7a240e79c76ea26d79ca3
Jorge Cadena <a01660252@tec.mx>
2025-08-16 18:42:01 -0600

    Finished register tests

--- api/tests/register.zsh
+++ api/tests/register.zsh
@@ -15,3 +15,12 @@ echo "\n---------------------------------------------\n"
 
 echo "Test 2: User already exists"
 echo $response_2
+
+echo "\n---------------------------------------------\n"
+
+response_3=`curl -s -X POST http://127.0.0.1:8000/v1/register \
+-H "Content-Type: application/json" \
+-d '{"email": "barbonia@barbo.com", "password": "barbo", "password_confirmation": "barbo"}'`
+
+echo "Test 3: Correct User Registration"
+echo $response_3
