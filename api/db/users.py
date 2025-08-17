e945b79d2915712b90ca5238801b2413f44fef90
Jorge Cadena <a01660252@tec.mx>
2025-08-17 03:14:45 -0600

    Created the add_children_addresses function

--- api/db/users.py
+++ api/db/users.py
@@ -1,4 +1,7 @@
 import pymongo
+from pymongo import ReturnDocument
+from bson import ObjectId
+from bson.errors import InvalidId
 from .connection import connect
 
 def add_user(user):
@@ -53,3 +56,24 @@ def get_user_password(email):
     user = users_collection.find_one({"email": email})
 
     return str(user["password"])
+
+def add_children_addresses(connector):
+    try:
+        client = connect()
+
+        db = client.Cluster0
+        users_collection = db["users"]
+
+        obj_id = ObjectId(connector.id)
+
+        updated_user = users_collection.find_one_and_update(
+            {"_id": obj_id},
+            {"$addToSet": {"children_addresses": connector.mac}},
+            return_document = ReturnDocument.AFTER
+        )
+
+        return updated_user
+    except InvalidId:
+        raise
+    except Exception as e:
+        raise
