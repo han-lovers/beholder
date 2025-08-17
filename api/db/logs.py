c352fb3f090b87944c3501a8bb52343000b17928
Jorge Cadena <a01660252@tec.mx>
2025-08-17 13:30:02 -0600

    Added the some DB function

--- api/db/logs.py
+++ api/db/logs.py
@@ -1,5 +1,7 @@
 from .connection import connect
 from datetime import datetime
+from bson import ObjectId
+from fastapi import HTTPException
 
 def add_logs(alert):
     client = connect()
@@ -13,3 +15,30 @@ def add_logs(alert):
 
     result = logs_collection.insert_one(alert_dict)
     return str(result.inserted_id)
+
+def get_alerts(id):
+    client = connect()
+
+    db = client.Cluster0
+
+    logs_collection = db["logs"]
+
+    try:
+        obj_id = ObjectId(id)
+    except Exception:
+        raise HTTPException(status_code=400, detail="Invalid ID format")
+
+    logs_cursor = logs_collection.find({"parent_id": id}, {"parent_id": 0})
+
+    logs = list(logs_cursor)
+
+    if not logs:
+        return []
+    for log in logs:
+        log["_id"] = str(log["_id"])
+        if "created_at" in log:
+            log["created_at"] = log["created_at"].isoformat()
+
+    return logs
+
+
